36aac2a5a8066ee8187bcac28f415ab7a8ced392
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2024-10-10 15:24:20 +0530

    Add files via upload

---
+++ Comments.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	// This is a commnet tline 
+	/*Here is a double line comment.
+	here we can have multiple line at once.*/
+}
---
+++ CreateVariable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int XYZ = 10;
+	
+	int ABC;
+	ABC = 15;
+
+	printf("%d",XYZ);
+	printf("%d",ABC);
+return 0;
+}
